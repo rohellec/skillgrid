06b5a4e59f6ea0d4ab43937a7b5b7bb21c7ae30c
Dima Rogov <ds.rogv@gmail.com>
2015-10-01 23:26:27 +0300

    Add additional support functions for RSpec tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,26 @@
 include ApplicationHelper
 
 def new_user
-  User.new(name: 'Example User', email: 'user@example.com')
+  User.new(name: 'Example User', email: 'user@example.com',
+           password: 'foobar', password_confirmation: 'foobar')
+end
+
+def fill_in_signup(user)
+  fill_in "Name",         with: user.name
+  fill_in "Email",        with: user.email
+  fill_in "Password",     with: user.password
+  fill_in "Confirmation", with: user.password
 end
 
 def duplicate_user(user)
   duplicate = user.dup
   duplicate.email = user.email.upcase
   duplicate.save
+end
+
+def save_with_options(user, options = {})
+  options.each { |key, value| user[key] = value }
+  user.save
 end
 
 def invalid_adresses
@@ -17,4 +30,16 @@ end
 
 def valid_adresses
   %w{ user@foo.COM A_US-EN@f.b.net frst.lst@foo.ru a+b@baz.fn }
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-danger', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
 end
